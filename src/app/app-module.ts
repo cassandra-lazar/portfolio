3b810f2752fc830b87cc36d703d5b6bc6b57db57
Cassandra Lazar <lazar_cassandra@yahoo.com>
2025-07-30 21:40:18 +0300

    add components

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -2,19 +2,26 @@ import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
-import { App } from './app';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutMeComponent } from './pages/home/sections/about-me/about-me.component';
+import { SkillsComponent } from './pages/home/sections/skills/skills.component';
+import { ProjectsComponent } from './pages/home/sections/projects/projects.component';
+import { ContactComponent } from './pages/home/sections/contact/contact.component';
+import { HeaderComponent } from './pages/home/sections/header/header.component';
 
 @NgModule({
   declarations: [
-    App
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
