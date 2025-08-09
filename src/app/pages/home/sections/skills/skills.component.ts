b21d85a6091f4dfeb52331a4a6bb0756d0ec5c0e
Cassandra Lazar <lazar_cassandra@yahoo.com>
2025-08-09 03:12:44 +0300

    skills

--- src/app/pages/home/sections/skills/skills.component.ts
+++ src/app/pages/home/sections/skills/skills.component.ts
@@ -61,34 +61,28 @@ export class SkillsComponent {
     },
   ];
 
-  allLogos = [
-    { name: 'HTML', logo: 'images/html.png' },
-    { name: 'CSS', logo: 'images/css.png' },
-    { name: 'SCSS', logo: 'images/scss.svg' },
-    { name: 'JavaScript', logo: 'images/javascript.png' },
-    { name: 'TypeScript', logo: 'images/typescript.svg' },
-    { name: 'Angular', logo: 'images/angular.svg' },
-    { name: 'PrimeNG', logo: 'images/primeng.svg' },
-    { name: 'PrimeFlex', logo: 'images/primeflex.svg' },
-    { name: 'Angular Material', logo: 'images/angular-material.svg' },
-    { name: 'Bootstrap', logo: 'images/bootstrap.svg' },
-    { name: 'Tailwind', logo: 'images/tailwind.svg' },
-    { name: 'Postman', logo: 'images/postman.svg' },
-    { name: 'RxJS', logo: 'images/rxjs.svg' },
-    { name: 'Jest', logo: 'images/jest.svg' },
-    { name: 'Karma', logo: 'images/karma.svg' },
-    { name: 'Jasmine', logo: 'images/asmine.svg' },
-    { name: 'Git', logo: 'images/git.svg' },
-    { name: 'GitHub', logo: 'images/github.svg' },
-    { name: 'npm & Node.js', logo: 'images/nodejs.svg' },
-    { name: 'WordPress', logo: 'images/wordpress.svg' },
-    { name: 'REST API', logo: 'images/api.svg' },
-    { name: 'Figma', logo: 'images/figma.svg' },
-    { name: 'Canva', logo: 'images/canva.svg' },
-    { name: 'JSON', logo: 'images/json.svg' },
-  ];
-
+  allLogos: any = [];
+  pauseBanner = false;
+  slideTrackWidth: number = 0;
+  slideWidth = 200;
   constructor(private displayService: DeviceDetectorService) {
     this.mobile = this.displayService.isMobile();
+  }
+
+  ngOnInit() {
+    this.allLogos = this.extractLogos();
+    this.slideTrackWidth = this.allLogos.length * this.slideWidth * 2;
+  }
+
+  extractLogos() {
+    return this.skills.flatMap((logo) => logo.items);
+  }
+
+  onMouseEnter() {
+    this.pauseBanner = true;
+  }
+
+  onMouseLeave() {
+    this.pauseBanner = false;
   }
 }
