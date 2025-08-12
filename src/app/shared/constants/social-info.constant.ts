2349fcab68ff57e4b4e55fbd8862cc8948266528
Cassandra Lazar <lazar_cassandra@yahoo.com>
2025-08-13 00:28:24 +0300

    enhancements

---
+++ src/app/shared/constants/social-info.constant.ts
@@ -0,0 +1,11 @@
+export const SOCIAL_INFO = {
+  NAME: 'Cassandra Lazar',
+  ROLE: 'Frontend Developer',
+  EMAIL: 'lazar.cassandra@gmail.com',
+  LOCATION: 'Bucharest, Romania',
+  TELEPHONE: '+40 725 156 106',
+  LINKEDIN: 'https://www.linkedin.com/in/cassandra-lazar-66077395/',
+  INSTAGRAM: 'https://www.instagram.com/cassandralazar',
+  GITHUB: 'https://github.com/cassy27/portfolio',
+  CV: '/CassandraLazar-CV.pdf',
+};
