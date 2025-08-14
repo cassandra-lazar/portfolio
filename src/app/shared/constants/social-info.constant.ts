ca79f2b673976ec426fe8d6a44c54a531f90935e
Cassandra Lazar <lazar_cassandra@yahoo.com>
2025-08-15 01:38:23 +0300

    change cv

--- src/app/shared/constants/social-info.constant.ts
+++ src/app/shared/constants/social-info.constant.ts
@@ -6,6 +6,6 @@ export const SOCIAL_INFO = {
   TELEPHONE: '+40 725 156 106',
   LINKEDIN: 'https://www.linkedin.com/in/cassandra-lazar-66077395/',
   INSTAGRAM: 'https://www.instagram.com/cassandralazar',
-  GITHUB: 'https://github.com/cassy27/portfolio',
-  CV: '/CassandraLazar-CV.pdf',
+  GITHUB: 'https://github.com/cassandra-lazar/portfolio',
+  CV: 'CassandraLazar-CV.pdf',
 };
