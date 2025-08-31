6eba728eb106d547d86f00e916bcfa9d93d8e64b
Cassandra Lazar <lazar_cassandra@yahoo.com>
2025-09-01 02:36:16 +0300

    update rm

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './sections/header/header.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { AboutMeComponent } from './sections/about-me/about-me.component';
+import { SkillsComponent } from './sections/skills/skills.component';
+import { ProjectsComponent } from './sections/projects/projects.component';
+import { ContactComponent } from './sections/contact/contact.component';
+import { TitleComponent } from '../../shared/components/title/title.component';
+import { CopyToClipboardDirective } from '../../shared/directives/copy-to-clipboard.directive';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +16,19 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent]
-    })
-    .compileComponents();
+      declarations: [
+        HomeComponent,
+        TitleComponent,
+        HeaderComponent,
+        HeroComponent,
+        AboutMeComponent,
+        SkillsComponent,
+        ProjectsComponent,
+        ContactComponent,
+        CopyToClipboardDirective,
+      ],
+      imports: [ReactiveFormsModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
--- src/app/pages/home/sections/about-me/about-me.component.spec.ts
+++ src/app/pages/home/sections/about-me/about-me.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutMeComponent } from './about-me.component';
+import { TitleComponent } from '../../../../shared/components/title/title.component';
 
 describe('AboutMeComponent', () => {
   let component: AboutMeComponent;
@@ -8,9 +8,8 @@ describe('AboutMeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AboutMeComponent]
-    })
-    .compileComponents();
+      declarations: [AboutMeComponent, TitleComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AboutMeComponent);
     component = fixture.componentInstance;
